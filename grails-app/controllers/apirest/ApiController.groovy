ecea0443d8d7469d94efd3a62146626a5b5ea2c1
PC_IMED\ASUS <imed.zaghdane.93@gmail.com>
2017-11-05 02:04:10 +0100

    Modification de APIcontroller et ajout des conditions

--- grails-app/controllers/apirest/ApiController.groovy
+++ grails-app/controllers/apirest/ApiController.groovy
@@ -8,79 +8,101 @@ class ApiController {
         def book() {
             switch (request.getMethod()){
                 case "GET":
-                    def book = Book.get(params.id)
-                    if(book)
-                    {
+                    if(params.id) {
+                        def book = Book.get(params.id)
+                        if (book) {
+                            withFormat {
+                                json { render book as JSON }
+                                xml { render book as XML }
+                            }
+                        } else {
+                            response.status = 404
+                            render "Not existing book!!."
+                        }
+                    }else {
                         withFormat {
-                            json { render book as JSON}
-                            xml { render book as XML}
+                            json { render Book.findAll() as JSON }
+                            xml { render Book.findAll() as XML }
                         }
                     }
-                    else
-                    {
-                        withFormat {
-                            json { render Book.findAll() as JSON}
-                            xml { render Book.findAll() as XML}
-                        }
-                    }
-
                     break;
                 case "POST":
                     def jsonObj = request.JSON
-                    //def book = new Book(jsonObj.name ,jsonObj.ISBN,jsonObj.author)
-                    def lib = Library.findById(jsonObj.library.id)
-                    //book.save(flush: true)
-                    lib.addToBooks(new Book(name: jsonObj.name,ISBN: jsonObj.ISBN,author: jsonObj.author,releaseDate: 2010))
-                    lib.save(flush: true)
+                    if(jsonObj.library){
+                        def lib = Library.findById(jsonObj.library.id)
+                        if(lib){
+                            lib.addToBooks(new Book(name: jsonObj.name,ISBN: jsonObj.ISBN,author: jsonObj.author,releaseDate: 2010))
+                            lib.save(flush: true)
 
-                    if(lib.save(flush: true)) {
-                        response.status = 201
-                        render "Successfully saved."
+                            if(lib.save(flush: true)) {
+                                response.status = 201
+                                render "Successfully saved."
+                            }
+                            else {
+                                render "Library Not saved."
+                                response.status = 400
+                            }
+                        }else{
+                            response.status = 404
+                            render "Not Existing library."
+                        }
+
+                    }else{
+                        render """You must include the ID of the library in the JSON body"""
                     }
-                    else
-                        render jsonObj.library.id
-                        response.status = 400
+
                     break;
                 case "PUT":
                     def jsonObj = request.JSON
-                    def book = Book.findById(params.id)
-
-                    if( jsonObj.name != null )
-                    {book.name = jsonObj.name}
-                    if( jsonObj.ISBN != null )
-                    {book.ISBN = jsonObj.ISBN}
-                    if( jsonObj.releaseDate != null )
-                    {book.releaseDate = new Date(jsonObj.releaseDate)}
-                    if( jsonObj.author != null )
-                    {book.author = jsonObj.author}
-
-                    if( jsonObj.library != null )
-                    {
-                        if(book.library != jsonObj.library){
-                            def lib = Library.findById(book.library.id)
-                            lib.removeFromBooks(book)
-                            def lib2 = Library.findById(jsonObj.library.id)
-                            if(lib2){
-                                lib2.addToBooks(book)
-                                book.save(flush: true)
-                                lib2.save(flash: true)
+                    if(params.id){
+                        def book = Book.findById(params.id)
+                        if(book) {
+                            if (jsonObj.name != null) {
+                                book.name = jsonObj.name
                             }
-                            else{
-                                render "Library not exist."
+                            if (jsonObj.ISBN != null) {
+                                book.ISBN = jsonObj.ISBN
                             }
+                            if (jsonObj.releaseDate != null) {
+                                book.releaseDate = new Date(jsonObj.releaseDate)
+                            }
+                            if (jsonObj.author != null) {
+                                book.author = jsonObj.author
+                            }
+
+                            if (jsonObj.library != null) {
+                                if (book.library != jsonObj.library) {
+                                    def lib = Library.findById(book.library.id)
+                                    lib.removeFromBooks(book)
+                                    def lib2 = Library.findById(jsonObj.library.id)
+                                    if (lib2) {
+                                        lib2.addToBooks(book)
+                                        book.save(flush: true)
+                                        lib2.save(flash: true)
+                                    } else {
+                                        render "Library not exist."
+                                    }
+                                }
+                            }
+
+                            book.save(flush: true)
+                            if (book.save()) {
+                                response.status = 200 // OK
+                                //render "Successfully update.\n"
+                                render book as JSON
+                            } else {
+                                response.status = 500 //Internal Server Error
+                                render "Could not update due to errors"
+                            }
+                        }else{
+                            response.status = 404 //Internal Server Error
+                            render "Book not found"
                         }
                     }
-
-                    book.save(flush: true)
-                    if(book.save()){
-                        response.status = 200 // OK
-                        //render "Successfully update.\n"
-                        render book as JSON
-                    }
                     else{
-                        response.status = 500 //Internal Server Error
-                        render "Could not create new Catalogo due to errors:\n ${lib.errors}"
+                        render """You must include the ID of the book!!"""
                     }
+
                     break
                 case "DELETE":
                     if(params.id){
@@ -93,12 +115,12 @@ class ApiController {
                         }
                         else{
                             response.status = 404 //Not Found
-                            render "${params.id} not found."
+                            render "Book not found."
                         }
                     }
                     else{
                         response.status = 400 //Bad Request
-                        render """DELETE request must include the ID code Example: /rest/library/id"""
+                        render """DELETE request must include the ID of the book"""
                     }
                     break
             //othrs case
@@ -113,41 +135,52 @@ class ApiController {
             switch (request.getMethod()) {
                 case "GET":
                     def lib = Library.get(params.id)
-                    def book = Book.get(params.idB)
-                    if(book)
-                    {
-                        if(lib.books.contains(book)) {
-                            withFormat {
-                                json { render book as JSON }
-                                xml { render book as XML }
+                    if(params.idB){
+                        def book = Book.get(params.idB)
+                        if(book)
+                        {
+                            if(lib.books.contains(book)) {
+                                withFormat {
+                                    json { render book as JSON }
+                                    xml { render book as XML }
+                                }
+                            }
+                            else{
+                                response.status = 404 //not found
+                                render """This book is not existing in the library!!"""
                             }
                         }
-                    }
-                    else {
+                        else {
+                            response.status = 404 //not found
+                            render """This book is not existing!!"""
+                        }
+                    }else {
                         withFormat {
                             json { render lib.books as JSON }
                             xml { render lib.books as XML }
                         }
                     }
+
                 break;
                 case "POST":
-                    if(!Library.get(params.id)){
-                        render (status: 400, text: "can't attach a book to a not existent library (${params.library.id})")
+                    if(!Library.findById(params.id)){
+                        render (status: 400, text: "can't attach a book to a not existent library")
                         return
                     }
 
                     def jsonObj = request.JSON
-                    def bookInstance = new Book(jsonObj)
-                    def lib = Library.get(params.id)
-                    //bookInstance.save(flush: true)
+                    def lib = Library.findById(params.id)
                     lib.addToBooks(new Book(name: jsonObj.name,ISBN: jsonObj.ISBN,author: jsonObj.author,releaseDate: new Date(jsonObj.releaseDate)))
                     lib.save(flush: true)
                     if(lib.save(flush:true)) {
                         response.status = 201
                         render "Successfully saved."
                     }
-                    else
+                    else{
                         response.status = 400
+                        render "Book not saved."
+                    }
+
                     break;
                 case "PUT":
                     def jsonObj = request.JSON
@@ -167,30 +200,35 @@ class ApiController {
                     lib.save(flush: true)
                     if(lib.save()){
                         response.status = 200 // OK
-                        render book as JSON
+                        render "Successfully updated."
                     }
                     else{
-                        response.status = 500 //Internal Server Error
-                        render "Could not create new Catalogo due to errors:\n ${lib.errors}"
+                        response.status = 400 //Internal Server Error
+                        render "Book not updated!!"
                     }
                     break
                 case "DELETE":
                     if(params.id){
-                        def lib = Library.findById(params.id)
-                        def book = Book.findById(params.idB)
-                        if(book){
-                            lib.removeFromBooks(book)
-                            book.delete(flush: true)
-                            render "Successfully Deleted."
+                        if(params.idB) {
+                            def lib = Library.findById(params.id)
+                            def book = Book.findById(params.idB)
+                            if (book) {
+                                lib.removeFromBooks(book)
+                                book.delete(flush: true)
+                                render "Successfully Deleted."
+                            } else {
+                                response.status = 404 //Not Found
+                                render "Book not found."
+                            }
                         }
                         else{
-                            response.status = 404 //Not Found
-                            render "${params.id} not found."
+                            response.status = 400 //Bad Request
+                            render """DELETE request must include the ID of the book"""
                         }
                     }
                     else{
                         response.status = 400 //Bad Request
-                        render """DELETE request must include the ID code Example: /rest/library/id"""
+                        render """DELETE request must include the IDof library"""
                     }
                     break
                 default:
@@ -211,54 +249,64 @@ class ApiController {
                     else
                     {
                         def lib = Library.get(params.id)
-                        withFormat {
-                            json { render lib as JSON}
-                            xml { render lib as XML}
+                        if(lib){
+                            withFormat {
+                                json { render lib as JSON}
+                                xml { render lib as XML}
+                            }
                         }
+                        else{
+                            render "Not Existing Library"
+                        }
+
                     }
 
                     break;
                 case "POST":
-                    /*def libInstance = new Library(params.library)
-                    if(libInstance.save(flush:true))
-                        render (status: 201, text: "library saved")
-                    else
-                        response.status = 400*/
                     def jsonObj = request.JSON
 
 
                     def lib = new Library(jsonObj)
-                    lib.save(flush: true)
-
                     if (!lib.save(flush: true)){
                         lib.errors.each {
                             println it
                         }
+                        render (status: 400, text: "Library not saved (Bad Request)")
+                    }else{
+                        lib.save(flush: true)
+                        render (status: 201, text: "Library saved")
                     }
-
-                    render  request.getJSON()
                     break;
             case "PUT":
                 def jsonObj = request.JSON
-                def lib = Library.findById(params.id)
+                if(params.id){
+                    def lib = Library.findById(params.id)
+                    if(lib){
+                        if( jsonObj.name != null )
+                        {lib.name = jsonObj.name}
 
-                    if( jsonObj.name != null )
-                    {lib.name = jsonObj.name}
+                        if( jsonObj.address != null )
+                        {lib.address = jsonObj.address}
 
-                    if( jsonObj.address != null )
-                    {lib.address = jsonObj.address}
+                        if( jsonObj.yearCreated != null )
+                        {lib.yearCreated = jsonObj.yearCreated}
 
-                    if( jsonObj.yearCreated != null )
-                    {lib.yearCreated = jsonObj.yearCreated}
-
-                lib.save(flush: true)
-                if(lib.save()){
-                    response.status = 200 // OK
-                    render lib as JSON
+                        if(lib.save(flush: true)){
+                            response.status = 200 // OK
+                            render lib as JSON
+                        }
+                        else{
+                            response.status = 500 //Internal Server Error
+                            render "Could not update  to errors:\n ${lib.errors}"
+                        }
+                    }else{
+                        response.status = 404
+                        render "This library is not existing"
+                    }
                 }
                 else{
-                    response.status = 500 //Internal Server Error
-                    render "Could not create new Catalogo due to errors:\n ${lib.errors}"
+                    response.status = 400 //Bad Request
+                    render """PUT request must include the ID of the library"""
                 }
                 break
             case "DELETE":
@@ -278,7 +326,7 @@ class ApiController {
                 }
                 else{
                     response.status = 400 //Bad Request
-                    render """DELETE request must include the ID code Example: /rest/catalogo/id"""
+                    render """DELETE request must include the ID of the library"""
                 }
                 break
                 default:
